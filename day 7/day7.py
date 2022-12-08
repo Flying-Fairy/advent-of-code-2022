ac190a9f9a08a05a8587f3694dc911aef28240c7
Flying-Fairy <axl.peroys@outlook.fr>
2022-12-08 22:30:27 +0100

    day 8 completed

---
+++ day 7/day7.py
@@ -0,0 +1,51 @@
+with open("day 7/input.txt") as f:
+    dir_stack = []
+    dir_sizes = {}
+    
+    for line in f.readlines():
+        line = line.split()
+    
+        if line[0] == "$":
+            if line[1] == "cd":
+                if line[2] != "..":
+                    if not dir_stack:
+                        dir_sizes[line[2]] = {"size": 0}
+                        dir_stack.append(dir_sizes[line[2]])
+                    else:
+                        dir_stack[-1][line[2]] = {"size": 0}
+                        dir_stack.append(dir_stack[-1][line[2]])
+                else:
+                    dir_stack.pop() 
+        
+        elif line[0] != "dir":
+            dir_stack[-1]["size"] += int(line[0])
+            
+def find_total(folder, ls):  # I suck at recursion but it works.
+    result = 0 
+    if len(folder) == 1:
+        ls.append(folder["size"])
+        return folder["size"]
+        
+    for k in folder:
+        if k != "size":
+            result += find_total(folder[k], ls)
+    
+    ls.append(result + folder["size"])
+    return result + folder["size"]
+    
+total_ls = []
+
+for folder in dir_sizes:
+    find_total(dir_sizes[folder], total_ls)
+
+answer = [i for i in total_ls if i <= 100000]
+print(sum(answer))
+
+# total space 70000000
+# needed for update 30000000         
+
+space_needed = (max(total_ls) + 30000000) - 70000000
+for i in sorted(total_ls):
+    if i >= space_needed:
+        print(i)
+        break
---
+++ day 8/day8.py
@@ -0,0 +1,91 @@
+with open("day 8/input.txt") as f:
+    tree_patch = []
+    for line in f.readlines():
+        tree_patch.append(list(line.strip()))
+
+visible_trees = 0
+
+def check_visible(ls, tree_pos, tree_height, direction):
+    x, y = tree_pos
+    is_visible = False
+    tree_score = {
+        "up":0,
+        "down": 0,
+        "left": 0,
+        "right": 0
+        }
+    
+    if direction == "up":
+        while x > 0:
+            x -= 1
+            current = ls[x][y]
+            if tree_height > current:
+                tree_score["up"] += 1
+                is_visible = True
+            else:
+                tree_score["up"] += 1
+                return False, tree_score["up"]
+        
+        return is_visible, tree_score["up"]
+
+    elif direction == "down":
+        while x < len(ls) - 1:
+            x += 1
+            current = ls[x][y]
+            if tree_height > current:
+                tree_score["down"] += 1
+                is_visible = True
+            else:
+                tree_score["down"] += 1
+                return False, tree_score["down"]
+        
+        return is_visible, tree_score["down"]
+    
+    elif direction == "left":
+        while y > 0:
+            y -= 1
+            current = ls[x][y]
+            if tree_height > current:
+                tree_score["left"] += 1
+                is_visible = True
+            else:
+                tree_score["left"] += 1
+                return False, tree_score["left"]
+        
+        return is_visible, tree_score["left"]
+
+    elif direction == "right":
+        while y < len(ls[x]) - 1:
+            y += 1
+            current = ls[x][y]
+            if tree_height > current:
+                tree_score["right"] += 1
+                is_visible = True
+            else:
+                tree_score["right"] += 1
+                return False, tree_score["right"]
+        
+        return is_visible, tree_score["right"]
+    
+scenic_scores = []
+
+for r_index, row in enumerate(tree_patch):
+    for c_index, col in enumerate(row):
+        pos = (r_index, c_index)
+        height = tree_patch[pos[0]][pos[1]]
+        if r_index == 0 or r_index == len(tree_patch) - 1 or c_index == 0 or c_index == len(row) - 1:
+            visible_trees += 1
+        else:
+            up, up_score = check_visible(tree_patch, pos, height, "up")
+            down, down_score = check_visible(tree_patch, pos, height, "down")
+            left, left_score = check_visible(tree_patch, pos, height, "left")
+            right, right_score = check_visible(tree_patch, pos, height, "right")
+            
+            if up or down or left or right:
+                visible_trees += 1
+            
+            score = up_score * down_score * left_score * right_score
+            scenic_scores.append(score)
+
+print(visible_trees)
+print(max(scenic_scores))
