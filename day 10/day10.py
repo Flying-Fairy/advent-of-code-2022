5ff891bd9f6fbeaee567455c3677c35dd7f2d215
Flying-Fairy <axl.peroys@outlook.fr>
2022-12-10 19:37:39 +0100

    add day 9 and 10

---
+++ day 10/day10.py
@@ -0,0 +1,43 @@
+# part 1
+x = 1
+cycle_number = 0
+target_cycles = [20, 60, 100, 140, 180, 220]
+cycle_result = []
+instructions = {
+    "addx": 2,
+    "noop": 1
+}
+# part 2
+crt_screen = [list("." * 40) for _ in range(6)]
+crt_lines = [40, 80, 120, 160, 200, 240]
+row = 0
+current_index = 0
+
+with open("day 10/input.txt") as f:
+    for line in f.readlines():
+        line = line.split()
+        
+        pixel_pos = (x - 1, x, x + 1)
+        
+        for i in range(instructions[line[0]]):
+            cycle_number += 1
+            
+            if current_index in pixel_pos:
+                if cycle_number <= crt_lines[row]:
+                    crt_screen[row][current_index] = "#"
+                
+            if cycle_number == crt_lines[row]:
+                current_index = 0
+                row += 1
+            else:
+                current_index += 1
+            
+            if cycle_number in target_cycles:
+                cycle_result.append(cycle_number * x)
+        
+            if line[0] == "addx" and i == 1:
+                x += int(line[1])
+
+print(sum(cycle_result))
+for i in crt_screen:
+    print(''.join(i))
---
+++ day 9/day9.py
@@ -0,0 +1,64 @@
+head = (0, 0)
+tail = (0, 0)
+
+traversed_pos = set()
+traversed_pos.add((0, 0))
+traversed_pos_rope = set()
+traversed_pos_rope.add((0, 0))
+
+rope = [(0, 0) for _ in range(10)]
+
+direction_dict = {"U": (1, 0),
+                  "D": (-1, 0),
+                  "R": (0, 1),
+                  "L": (0, -1)}
+
+with open("day 9/input.txt") as f:
+    for line in f.readlines():
+        direction, distance = line.split()[0], int(line.split()[1])
+        direction_x, direction_y = direction_dict[direction]
+        
+        for i in range(distance):
+            head_x, head_y = head
+            head_x, head_y = head_x + direction_x, head_y + direction_y
+            
+            if abs(head_x - tail[0]) >= 2 or abs(head_y - tail[1]) >= 2:
+                tail = head
+                traversed_pos.add(tail)
+            
+            head = (head_x, head_y)
+                
+print(len(traversed_pos))
+
+with open("day 9/input.txt") as f:
+    for line in f.readlines():
+        direction, distance = line.split()[0], int(line.split()[1])
+        direction_x, direction_y = direction_dict[direction]
+        
+        for i in range(distance):
+            head_x, head_y = rope[0]
+            head_x, head_y = head_x + direction_x, head_y + direction_y
+            rope[0] = (head_x, head_y)
+            
+            for j in range(9):
+                hx, hy = rope[j]
+                tail_x, tail_y = rope[j + 1]
+                
+                if abs(hx - tail_x) >= 2 or abs(hy - tail_y) >= 2:
+                    diff_x, diff_y = 1, 1
+                    
+                    if hx - tail_x == 0:
+                        diff_x = 0
+                    elif hx - tail_x < 0:
+                        diff_x = -1
+                    if hy - tail_y == 0:
+                        diff_y = 0
+                    elif hy - tail_y < 0:
+                        diff_y = -1
+                                     
+                    rope[j + 1] = (tail_x + diff_x, tail_y + diff_y)
+                    
+                    if j == 8:
+                        traversed_pos_rope.add(rope[j + 1])
+                       
+print(len(traversed_pos_rope))        
